f7c86b7daebe764b341712809d8a34acf881ad8a
zhihuac-tku <zhihua0354@gmail.com>
2026-04-30 14:00:30 +0800

    Update OfficeHour_Matcher-ver2.py

--- OfficeHour_Matcher-ver2.py
+++ OfficeHour_Matcher-ver2.py
@@ -482,11 +482,8 @@ else:
                 
             original_ts = str(row["timestamp"])
             
-            try:
-                original_ts = str(row["timestamp"])
-                
+            try:    
                 save_final(
-                    original_ts,
                     search_id,
                     t_a,
                     t_b,
